36bf09d9dc06aeb8a3e1c4d2eb2405dfae791f45
teddy <liaowd@sustc.edu.cn>
2017-05-26 21:29:36 +0800

    Revert "need to be deleted"
    This reverts commit 66ae8d242c3a1e616c1b0b6f82c6ba86a19920f6.

--- app/src/main/java/com/sustech/se/scoree/Audio2Key_refactor.java
+++ app/src/main/java/com/sustech/se/scoree/Audio2Key_refactor.java
@@ -22,8 +22,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 
-import devlight.io.library.ntb.NavigationTabBar;
-
 
 public class Audio2Key_refactor extends AppCompatActivity{
 
